2778e803d282ad64e35c4ad98342bc684f3a57db
dereklee0310 <dereklee0310.gmail.com>
2022-07-12 00:57:27 +0800

    add init files

--- cmds/event.py
+++ cmds/event.py
@@ -2,7 +2,7 @@
 Author: dereklee0310 dereklee0310.gmail.com
 Date: 2022-02-20 15:40:41
 LastEditors: dereklee0310 dereklee0310.gmail.com
-LastEditTime: 2022-07-11 16:56:08
+LastEditTime: 2022-07-12 00:37:32
 FilePath: \discord_bot_repo\cmds\event.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -19,6 +19,7 @@ from pathlib import Path
 with open ('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(jdata['func_lib']) 
 # # from func import is_text_channel
 import func
